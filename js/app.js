77edae2a160286b83dca25cd6f592afcaa9b1005
Haur Kang <haur.kang@gmail.com>
2019-12-17 21:48:04 +0000

    add toggle btn to show and hide the navbar

--- js/app.js
+++ js/app.js
@@ -5,3 +5,9 @@
 window.addEventListener('load', function() {
     document.querySelector('.preloader').style.display='none';
 })
+
+// toggle nav bar
+
+document.querySelector('.navBtn').addEventListener('click', function() {
+    document.querySelector('.nav').classList.toggle('nav--show');
+})
