76d39b3382caf33e59ccfbab662a17c131879fd9
Haur Kang <haur.kang@gmail.com>
2020-01-04 23:15:23 +0000

    add js to form

--- js/app.js
+++ js/app.js
@@ -16,6 +16,28 @@ function eventListeners() {
     document.querySelector('.video__switch').addEventListener('click', function () {
         ui.videoControls()
     })
+    // submit the form
+    document.querySelector('.drink-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const name = document.querySelector('.input-name').value;
+        const lastName = document.querySelector('.input-lastname').value;
+        const email = document.querySelector('.input-email').value;
+
+        let value = ui.checkEmpty(name, lastName, email)
+
+        if (value) {
+            let customer = new Customer(name, lastName, email)
+            console.log(customer);
+            ui.addCustomer(customer)
+            ui.showFeedback('custumer added to the list', 'success')
+            ui.clearFields()
+
+        }
+        else {
+            ui.showFeedback('some form values empty', 'error')
+        }
+
+    })
 }
 
 
@@ -42,5 +64,33 @@ UI.prototype.videoControls = function () {
     else {
         btn.classList.remove('btnSlide')
         document.querySelector('.video__item').play()
+    }
+}
+
+// check for empty values
+UI.prototype.checkEmpty = function (name, lastname, email) {
+    let result;
+    if (name === '' || lastname === '' || email === '') {
+
+        result = false;
+    }
+    else {
+        result = true;
+    }
+    return result;
+}
+
+UI.prototype.showFeedback = function (text, type) {
+    const feedback = document.querySelector('.drink-form__feedback');
+    if (type === 'success') {
+        feedback.classList.add('success');
+        feedback.innerText = text;
+        this.removeAlert('success');
+    }
+    else if (type === 'error') {
+
+        feedback.classList.add('error');
+        feedback.innerText = text;
+        this.removeAlert('error');
     }
 }
